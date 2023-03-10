777437e7e9ef26bcb77c40af01961fa89a0941c7
Sandy Bui <stbui_emory@yahoo.com>
2023-03-10 00:42:57 -0500

    addl search results page styling

--- assets/searchresultspage.js
+++ assets/searchresultspage.js
@@ -1,14 +1,24 @@
-function changeImage() {
-    var img = document.getElementById("myImg");
-    img.setAttribute("src", "assets/save favorite after.png");
-}
+// Function that toggles Save this Pairing icon on clicks
+function changeImage(img) {
+    if (img.getAttribute('src') === './assets/save favorite before.png') {
+      img.setAttribute('src', './assets/save favorite after.png');
+    } else {
+      img.setAttribute('src', './assets/save favorite before.png');
+    }
+  }  
 
-function changeImage2() {
-    var img = document.getElementById("myImg2");
-    img.setAttribute("src", "assets/save favorite after.png");
-}
+  function changeImage2(img) {
+    if (img.getAttribute('src') === './assets/save favorite before.png') {
+      img.setAttribute('src', './assets/save favorite after.png');
+    } else {
+      img.setAttribute('src', './assets/save favorite before.png');
+    }
+  }  
 
-function changeImage3() {
-    var img = document.getElementById("myImg3");
-    img.setAttribute("src", "assets/save favorite after.png");
-}
+  function changeImage3(img) {
+    if (img.getAttribute('src') === './assets/save favorite before.png') {
+      img.setAttribute('src', './assets/save favorite after.png');
+    } else {
+      img.setAttribute('src', './assets/save favorite before.png');
+    }
+  }  
